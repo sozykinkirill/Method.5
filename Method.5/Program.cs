a64ad78137955f3278ffd562986e9fe6b8f2d337
sozykinkirill <kirill.sozykin159@yandex.ru>
2023-04-12 16:01:51 +0500

    Добавьте файлы проекта.

---
+++ Method.5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Method
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string saidworld, int deep)
+        {
+            Console.WriteLine(saidworld);
+
+            var modif = saidworld;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+            Console.WriteLine("..." + modif);
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+
+
+        }
+    }
+}
